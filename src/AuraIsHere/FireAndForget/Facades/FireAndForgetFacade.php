41521c5151fad9b5bd33b6b5fd8c3a5acedaf64e
Caleb Fidecaro <calebfidecaro@gmail.com>
2014-11-05 14:50:18 +1300

    Add facade

---
+++ src/AuraIsHere/FireAndForget/Facades/FireAndForgetFacade.php
@@ -0,0 +1,16 @@
+<?php namespace AuraIsHere\FireAndForget\Facades;
+
+use Illuminate\Support\Facades\Facade;
+
+class FireAndForgetFacade extends Facade
+{
+	/**
+	 * Get the registered name of the component.
+	 *
+	 * @return string
+	 */
+	protected static function getFacadeAccessor()
+	{
+		return 'fire-and-forget';
+	}
+}
